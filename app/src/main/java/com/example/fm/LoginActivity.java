329e92aee27defad041bb52de3b05f9bb55595cc
ishapugalia <jain16isha@gmail.com>
2020-05-09 20:04:48 +0530

    commit

---
+++ app/src/main/java/com/example/fm/LoginActivity.java
@@ -0,0 +1,122 @@
+package com.example.fm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class LoginActivity extends AppCompatActivity {
+
+    //Variables
+    Button callSignup,logIn;
+    ImageView image;
+    TextView logoText,sloganText;
+    TextInputLayout username,passwd;
+
+    //Firebase
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Hooks
+        callSignup = findViewById(R.id.signup);
+        logIn = findViewById(R.id.login);
+        image = findViewById(R.id.logoImage);
+        logoText = findViewById(R.id.logo_name);
+        sloganText = findViewById(R.id.slogan_name);
+        username = findViewById(R.id.username);
+        passwd = findViewById(R.id.password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+        callSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,Signup.class);
+
+                Pair[] pairs = new Pair[7];
+
+                pairs[0] = new Pair<View,String>(image,"logo_image");
+                pairs[1] = new Pair<View,String>(logoText,"logo_text");
+                pairs[2] = new Pair<View,String>(sloganText,"logo_desc");
+                pairs[3] = new Pair<View,String>(username,"username_trans");
+                pairs[4] = new Pair<View,String>(passwd,"password_trans");
+                pairs[5] = new Pair<View,String>(logIn,"button_trans");
+                pairs[6] = new Pair<View,String>(callSignup,"signup_trans");
+
+
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this,pairs);
+                startActivity(i,options.toBundle());
+
+
+            }
+        });
+    }
+
+    public void userLogin(View view){
+
+        String email,password;
+        email = username.getEditText().getText().toString().trim();
+        password = passwd.getEditText().getText().toString().trim();
+
+        if(email.isEmpty()) {
+            username.setError("Email is required!");
+            username.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            username.setError("Email is invalid!");
+            username.requestFocus();
+            return;
+        }
+        if(password.isEmpty()) {
+            passwd.setError("Password is required!");
+            passwd.requestFocus();
+            return;
+        }
+        if(password.length()<6) {
+            passwd.setError("Password is too short!");
+            passwd.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+               if(task.isSuccessful()){
+                   startActivity(new Intent(LoginActivity.this,Options.class));
+
+               }
+               else{
+                   Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+               }
+            }
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/fm/Signup.java
@@ -0,0 +1,145 @@
+package com.example.fm;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.lang.Throwable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Signup extends AppCompatActivity {
+
+    //Variables
+    TextInputLayout editName,editUsername,editEmail,editPhnumber,editPassword;
+    Button register,login;
+     String ownerid;
+     public static final String TAG ="TAG";
+    //Firebase
+    private FirebaseAuth mAuth;
+    //FirebaseDatabase rootNode;
+    //DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        //Hooks
+        editName = findViewById(R.id.name);
+        editUsername = findViewById(R.id.username);
+        editEmail = findViewById(R.id.email);
+        editPassword = findViewById(R.id.password);
+        editPhnumber = findViewById(R.id.phone);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.haveacc);
+
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseFirestore fstore= FirebaseFirestore.getInstance();
+
+       // register.setOnClickListener((View.OnClickListener) this);
+        //UserHelperClass helperClass = new UserHelperClass(name,username,email,phone,password);
+            //reference.child(username).setValue(helperClass));
+
+           /* final String name = editName.getEditText().getText().toString().trim();
+            final String username = editUsername.getEditText().getText().toString().trim();
+            final String email = editEmail.getEditText().getText().toString().trim();
+            final String phone = editPhnumber.getEditText().getText().toString().trim();
+            String password = editPassword.getEditText().getText().toString().trim();*/
+    register.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            final String name, username, email, phone, password;
+            name = editName.getEditText().getText().toString().trim();
+            username = editUsername.getEditText().getText().toString().trim();
+            email = editEmail.getEditText().getText().toString().trim();
+            phone = editPhnumber.getEditText().getText().toString().trim();
+            password = editPassword.getEditText().getText().toString().trim();
+            if (email.isEmpty()) {
+                editEmail.setError("Email is required!");
+                editEmail.requestFocus();
+                return;
+            }
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                editEmail.setError("Email is invalid!");
+                editEmail.requestFocus();
+                return;
+            }
+            if (password.isEmpty()) {
+                editPassword.setError("Password is required!");
+                editPassword.requestFocus();
+                return;
+            }
+            if (password.length() < 6) {
+                editPassword.setError("Password is too short!");
+                editPassword.requestFocus();
+                return;
+            }
+
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "User Registered Successfully", Toast.LENGTH_SHORT).show();
+                        ownerid = mAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fstore.collection("owners").document(ownerid);
+                        Map<String, Object> owner = new HashMap<>();
+                        owner.put("fname", name);
+                        owner.put("uname", username);
+                        owner.put("phone", phone);
+                        owner.put("email", email);
+                        documentReference.set(owner).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "owner profile is created!" + ownerid);
+                            }
+                        });
+                        startActivity(new Intent(Signup.this, Options.class));
+                    } else {
+
+                        if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                            Toast.makeText(getApplicationContext(), "Account already exists!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+
+            });
+        }
+    });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, LoginActivity.class));
+            }
+
+
+
+            });
+
+
+    }
+}
+
+
