54c5cbb23a577f8e35fefe3ae144be0a9f3c530e
Chethan30 <chetan611611611@gmail.com>
2020-05-10 16:30:14 +0530

    update today

--- app/src/main/java/com/example/fm/Goods.java
+++ app/src/main/java/com/example/fm/Goods.java
@@ -1,32 +1,29 @@
 package com.example.fm;
 
-import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
+public class Goods {
+    String productName;
+    String type;
+    int pieces;
+    int boxes;
+    Double weight;
+    boolean fragile;
 
-public class Goods extends AppCompatActivity {
 
-    public Goods(String productName, String type, int pieces, int boxes, double weight,boolean fragile) {
+    Goods( ){
+        this.productName = "";
+        this.boxes = 0;
+        this.pieces = 0;
+        this.weight = 0.00;
+        this.fragile = false;
+
     }
+    Goods(String productName,String type,int pieces,int boxes,Double weight,boolean fragile){
+        this.productName = productName;
+        this.boxes = boxes;
+        this.pieces = pieces;
+        this.weight = weight;
+        this.fragile = fragile;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goods);
-       /* Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
     }
-
 }
