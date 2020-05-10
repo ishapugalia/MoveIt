2dbb51660a6363e150e327e78f46d2e619eb5889
Chethan30 <chetan611611611@gmail.com>
2020-05-10 17:54:06 +0530

    update today

--- app/src/main/java/com/example/fm/Goods.java
+++ app/src/main/java/com/example/fm/Goods.java
@@ -9,21 +9,21 @@ public class Goods {
     boolean fragile;
 
 
-    Goods( ){
+    Goods(){
         this.productName = "";
         this.boxes = 0;
         this.pieces = 0;
         this.weight = 0.00;
+        this.type = "";
         this.fragile = false;
-
     }
+
     Goods(String productName,String type,int pieces,int boxes,Double weight,boolean fragile){
         this.productName = productName;
         this.boxes = boxes;
+        this.type = type;
         this.pieces = pieces;
         this.weight = weight;
         this.fragile = fragile;
-
-
     }
 }
--- app/src/main/java/com/example/fm/Newgoods.java
+++ app/src/main/java/com/example/fm/Newgoods.java
@@ -3,15 +3,20 @@ package com.example.fm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Newgoods extends AppCompatActivity {
@@ -19,13 +24,16 @@ public class Newgoods extends AppCompatActivity {
     //Variables
     TextInputLayout prodname,numofP,numofB,typeof,weight1;
     String ownerid;
-     boolean fragile;
-
+    Switch frag;
+    public static final String TAG ="TAG";
+     //boolean fragile;
      Button register;
+
     //Firebase
     private FirebaseAuth mAuth;
     private FirebaseFirestore firestore;
-    private CollectionReference colref;
+    //private CollectionReference colref;
+    private DocumentReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,39 +45,68 @@ public class Newgoods extends AppCompatActivity {
         numofB = findViewById(R.id.numb);
         typeof = findViewById(R.id.typeof);
         weight1 = findViewById(R.id.weight);
-         fragile= ((Switch)findViewById(R.id.fragile)).isChecked();
-         register=findViewById(R.id.register);
+        frag = findViewById(R.id.fragile);
+        register=findViewById(R.id.register);
+
+
         //Instance linking
         mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
-         colref = firestore.collection("owners");
-    }
-
-    public void addGoods(View view){
-        final String productName,type;
-        final int pieces,boxes;
-        final double weight;
-
-        productName = prodname.getEditText().getText().toString().trim();
-        type = typeof.getEditText().getText().toString().trim();
-        pieces = Integer.parseInt(numofP.getEditText().getText().toString().trim());
-        boxes = Integer.parseInt(numofB.getEditText().getText().toString().trim());
-        weight = Double.parseDouble(weight1.getEditText().getText().toString().trim());
-
-        if (productName.isEmpty()) {
-            prodname.setError("Name is required!");
-            prodname.requestFocus();
-            return;
-        }
-        if (type.isEmpty()) {
-            typeof.setError("Type is required!");
-            typeof.requestFocus();
-            return;
-        }
-
         ownerid = mAuth.getCurrentUser().getUid();
-        Goods newg= new Goods(productName,type,pieces,boxes,weight,fragile);
-        colref.document(ownerid).collection("Goods").add(newg);
+        db = firestore.collection("owners").document(ownerid);
+         //colref = firestore.collection("owners");
 
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String productName,type;
+                final int pieces,boxes;
+                final double weight;
+                final boolean res;
+
+                productName = prodname.getEditText().getText().toString().trim();
+                type = typeof.getEditText().getText().toString().trim();
+                pieces = Integer.parseInt(numofP.getEditText().getText().toString().trim());
+                boxes = Integer.parseInt(numofB.getEditText().getText().toString().trim());
+                weight = Double.parseDouble(weight1.getEditText().getText().toString().trim());
+                res = frag.isChecked();
+
+                if (productName.isEmpty()) {
+                    prodname.setError("Name is required!");
+                    prodname.requestFocus();
+                    return;
+                }
+                if (type.isEmpty()) {
+                    typeof.setError("Type is required!");
+                    typeof.requestFocus();
+                    return;
+                }
+
+                ownerid = mAuth.getCurrentUser().getUid();
+
+                Goods newg= new Goods(productName,type,pieces,boxes,weight,res);
+                db.collection("Goods").add(newg);
+                Map<String, Object> good = new HashMap<>();
+                good.put("pname", productName);
+                good.put("type", type);
+                good.put("numb", boxes);
+                good.put("nump", pieces);
+                good.put("weight", weight);
+                good.put("fragile", res);
+                db.set(good).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "Object is added!");
+                    }
+                });
+
+
+                //colref.document(ownerid).collection("Goods").add();
+
+
+            }
+        });
     }
+
+
 }
