87da77d2de0f32a6526677d0a06bf98f08805de4
ishapugalia <jain16isha@gmail.com>
2020-05-11 21:35:03 +0530

    swipe to delete

--- app/src/main/java/com/example/fm/Listgoods.java
+++ app/src/main/java/com/example/fm/Listgoods.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -28,19 +29,17 @@ public class Listgoods extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private RecyclerView mFirestorelist;
     private CollectionReference colref;
-    FirestoreRecyclerAdapter adapter;
+    private GoodsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listgoods);
-
         mFirestorelist = findViewById(R.id.firestore_list);
 
         //INstancing
         mAuth = mAuth.getInstance();
         firestore = firestore.getInstance();
-
         ownerid = mAuth.getCurrentUser().getUid();
         colref = firestore.collection("owners");
 
@@ -49,52 +48,30 @@ public class Listgoods extends AppCompatActivity {
 
         //RecyclerOptions
         FirestoreRecyclerOptions<Goodsmodel> options = new FirestoreRecyclerOptions.Builder<Goodsmodel>()
-                .setQuery(query,Goodsmodel.class).build();
-        adapter = new FirestoreRecyclerAdapter<Goodsmodel, Goodsviewholder>(options) {
-            @NonNull
-            @Override
-            public Goodsviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singleitem,parent,false);
-                return new Goodsviewholder(view);
-            }
+                .setQuery(query, Goodsmodel.class).build();
 
-            @SuppressLint("SetTextI18n")
-            @Override
-            protected void onBindViewHolder(@NonNull Goodsviewholder holder, int position, @NonNull Goodsmodel model) {
-                holder.name.setText(model.getPname());
-                holder.type.setText(model.getType());
-               holder.nump.setText(model.getPieces()+" ");
-                holder.numb.setText(model.getBoxes()+" ");
-                holder.weight.setText((int) model.getWeight()+"");
-                holder.fragile.setText(String.valueOf(model.isFragile()));
-            }
-        };
+          adapter=new GoodsAdapter(options);
 
         mFirestorelist.setHasFixedSize(true);
         mFirestorelist.setLayoutManager(new LinearLayoutManager(this));
         mFirestorelist.setAdapter(adapter);
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
 
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                adapter.deleteItem(position);
 
-
+            }
+        }).attachToRecyclerView(mFirestorelist);
         //ViewHolder
-        }
-
-    private class Goodsviewholder extends  RecyclerView.ViewHolder{
-
-        private TextView name,type,nump,numb,weight,fragile;
-
-        public Goodsviewholder(@NonNull View itemView) {
-            super(itemView);
-
-            name = itemView.findViewById(R.id.Rename);
-            type = itemView.findViewById(R.id.type);
-           nump = itemView.findViewById(R.id.pieces);
-           numb = itemView.findViewById(R.id.boxes);
-            weight = itemView.findViewById(R.id.weight);
-            fragile=itemView.findViewById((R.id.fragility));
-
-        }
     }
+
+
 
     @Override
     protected void onStop() {
@@ -108,4 +85,54 @@ public class Listgoods extends AppCompatActivity {
         adapter.startListening();
     }
 }
+class GoodsAdapter extends FirestoreRecyclerAdapter<Goodsmodel, GoodsAdapter.Goodsviewholder> {
+
+    public GoodsAdapter(@NonNull FirestoreRecyclerOptions<Goodsmodel> options) {
+        super(options);
+    }
+
+    public Goodsviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.singleitem, parent, false);
+        return new Goodsviewholder(view);
+    }
+
+    class Goodsviewholder extends RecyclerView.ViewHolder {
+
+        private TextView name, type, nump, numb, weight, fragile;
+
+        public Goodsviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            name = itemView.findViewById(R.id.Rename);
+            type = itemView.findViewById(R.id.type);
+            nump = itemView.findViewById(R.id.pieces);
+            numb = itemView.findViewById(R.id.boxes);
+            weight = itemView.findViewById(R.id.weight);
+            fragile = itemView.findViewById((R.id.fragility));
+
+        }
+    }
+
+    public void deleteItem(int position) {
+        getSnapshots().getSnapshot(position).getReference().delete();
+    }
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onBindViewHolder(@NonNull Goodsviewholder holder, int position, @NonNull Goodsmodel model) {
+        holder.name.setText(model.getPname());
+        holder.type.setText(model.getType());
+        holder.nump.setText(model.getPieces() + " ");
+        holder.numb.setText(model.getBoxes() + " ");
+        holder.weight.setText((int) model.getWeight() + "");
+        holder.fragile.setText(String.valueOf(model.isFragile()));
+    }
+
+          /*  public void deleteItem(int position) {
+                getSnapshots().getSnapshot(position).getReference().delete();
+            }*/
+}
+
+;
+
+
 
